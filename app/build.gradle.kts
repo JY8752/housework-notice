c43fbec731754629d127acf9b24ed3918297095a
JY8752 <j.y.8752@gmail.com>
2022-04-03 00:01:39 +0900

    github actions作成

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.6.10"
+    application
 }
 
 repositories {
@@ -31,4 +32,8 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+application {
+    mainClass.set("housework.notice.AppKt")
 }
